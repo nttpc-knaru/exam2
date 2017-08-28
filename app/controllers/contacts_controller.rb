e6cb92aeefb09d4f5150dd49b365d066055f17c0
koichi <knaru1977@gmail.com>
2017-08-28 05:09:56 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,34 @@
+class ContactsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    if params[:back]
+      @contact = Contact.new(contacts_params)
+    else
+      @contact = Contact.new
+    end
+  end
+
+  def create
+  #Contact.create(contacts_params)
+  #redirect_to new_contact_path
+  @contact=Contact.create(contacts_params)
+    if @contact.save
+      redirect_to contacts_path, notice: "お問い合わせありがとうございました！"
+    else
+      render 'new'
+    end
+  end
+
+  def confirm
+    @contact = Contact.new(contacts_params)
+    render :new if @contact.invalid?
+  end
+
+  private
+    def contacts_params
+      params.require(:contact).permit(:name,:email,:content)
+    end
+end
