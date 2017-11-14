12230f0227994461b59f53947572719c0399c2e9
koichi <knaru1977@gmail.com>
2017-11-14 05:11:48 +0000

    20171114

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,7 +14,7 @@ class ContactsController < ApplicationController
   def create
   #Contact.create(contacts_params)
   #redirect_to new_contact_path
-  @contact=Contact.create(contacts_params)
+  @contact = Contact.create(contacts_params)
     if @contact.save
       redirect_to root_path, notice: "お問い合わせが完了しました！"
       NoticeMailer.sendmail_contact(@contact).deliver
