59f06cb3018883701296b9bdbcfa1f8495c81b37
koichi <knaru1977@gmail.com>
2017-11-14 15:16:37 +0000

    20171115

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -14,8 +14,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_contact(contact)
     @contact = contact
 
-    #mail from: "k-naru@nttpc.co.jp",
-    mail to: "k-naru@nttpc.co.jp",
+    mail to: @contact.email,
         subject: '[Achieve] 問い合わせしました'
   end
 end
