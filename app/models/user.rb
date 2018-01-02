38f104c58007ce23a5fd3f6f653d1e1ff28959ef
koichi <knaru1977@gmail.com>
2018-01-02 15:56:08 +0000

    DIVE14

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,24 @@ class User < ActiveRecord::Base
    user
   end
 
+  def self.find_for_twitter_oauth(auth,signed_in_resource=nil)
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+
+   unless user
+     user = User.new(
+       name: auth.extra.raw_info.name,
+       provider: auth.provider,
+       uid: auth.uid,
+       email: "#{auth.uid}-#{auth.provider}@example.com",
+       image_url: auth.info.image,
+       password: Devise.friendly_token[0,20]
+     )
+     user.skip_confirmation!
+     user.save(validate: false)
+   end
+   user
+  end
+
   def self.create_unique_string
     SecureRandom.uuid
   end
