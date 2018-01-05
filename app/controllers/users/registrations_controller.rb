6381a151eb5b3943f05ae78f14a95a89c2d50fa7
koichi <knaru1977@gmail.com>
2018-01-05 15:08:58 +0000

    20180105

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def build_resource(hash=nil)
-    hash[:uid] = User.create_unique_string
+    #hash[:uid] = User.create_unique_string
+    User.create_unique_string
     super
   end
 end
